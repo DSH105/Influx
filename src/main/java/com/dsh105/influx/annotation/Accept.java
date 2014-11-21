89f81a8579f6bb327eeed6a4d6da1d9a0b4d49b8
DSH105 <jason.chirgwin@icloud.com>
2014-11-21 21:34:07 +1100

    Assign a default value for the Accept annotation

--- src/main/java/com/dsh105/influx/annotation/Accept.java
+++ src/main/java/com/dsh105/influx/annotation/Accept.java
@@ -26,7 +26,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.PARAMETER)
 public @interface Accept {
 
-    public int value();
+    public int value() default 1;
 
     public String showAs() default "";
 }
